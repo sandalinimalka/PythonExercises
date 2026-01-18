ccc5c696f78e293efd23c58e4443376b4cd635c0
Sandali Nimalka <sandalinimalka@outlook.com>
2026-01-19 00:53:04 +0530

    Exercise 02 part 04

---
+++ Exercise 02/question4.py
@@ -0,0 +1,15 @@
+number1_str = input("Enter a First Number: ")
+number2_str = input("Enter a Second Number: ")
+number3_str = input("Enter a Third Number: ")
+
+number1 = int(number1_str)
+number2 = int(number2_str)
+number3 = int(number3_str)
+
+summation = (number1 + number2 + number3)
+product = (number1 * number2 * number3)
+average = ((number1 + number2 + number3) / 3)
+
+print("Sum is " + str(summation))
+print("Product is " + str(product))
+print("Average is" + str(average))
