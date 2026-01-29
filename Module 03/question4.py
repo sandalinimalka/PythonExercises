514a57b6b8f6d96928c1f06b6a2b3e92f260721e
Sandali Nimalka <sandalinimalka@outlook.com>
2026-01-29 12:49:00 +0530

    Question4 redo

--- Module 03/question4.py
+++ Module 03/question4.py
@@ -1,10 +1,6 @@
 year = int(input("Enter a year: "))
 
-if year % 400 == 0:
-    print("It is a Leap Year")
-elif year % 100 == 0:
-    print("It is not a Leap Year")
-elif year % 4 == 0:
+if year % 400 == 0 and (year % 100 != 0 or year % 400 == 0):
     print("It is a Leap Year")
 else:
     print("It is not a Leap Year")
